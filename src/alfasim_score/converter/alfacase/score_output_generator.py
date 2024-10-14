f2f31821ac208583d02aa4aed9eb2491de61ee0a
Gabriel Antão <gabrielfoa@esss.com>
2024-10-14 18:02:40 -0300

    Add data for the tubing pressure and temperature;
    PWPA-1901

--- src/alfasim_score/converter/alfacase/score_output_generator.py
+++ src/alfasim_score/converter/alfacase/score_output_generator.py
@@ -21,43 +21,36 @@ from alfasim_score.units import LENGTH_UNIT
 from alfasim_score.units import PRESSURE_UNIT
 from alfasim_score.units import TEMPERATURE_UNIT
 
-# TODO: move this to the generate_output_results
-EXPECTED_PROFILES = [
-    "pressure",
-    "mixture temperature",
-    # "interface_19_temperature",
-]
 
-
-# TODO: put this in a result generator class to convert alfasim result to SCORE
 def generate_output_results(
     results_path: Path, element_name: str = WELLBORE_NAME, active_annuli: list[str] = ["a"]
 ) -> Dict[str, Any]:
+    results = Results(results_path)
     annuli_temperature_profiles = [
         f"annulus_{annuli_label}_temperature" for annuli_label in active_annuli
     ]
     annuli_pressure_profiles = [
         f"annulus_{annuli_label}_pressure" for annuli_label in active_annuli
     ]
-
-    results = Results(results_path)
-    global_trends_list = [str(trend) for trend in results.list_global_trends()]
-
-    time_step = results.get_global_trend_curve("timestep")
-    count = len(time_step.image)
-    expected_profiles_list = [
-        f"{profile_name}@{element_name}(timesteps={count})" for profile_name in EXPECTED_PROFILES
+    tubing_profiles = [
+        "pressure",
+        "mixture temperature",
     ]
 
     # TODO: check if need this
+    # global_trends_list = [str(trend) for trend in results.list_global_trends()]
+    # time_step = results.get_global_trend_curve("timestep")
+    # count = len(time_step.image)
+    # expected_profiles_list = [
+    #     f"{profile_name}@{element_name}(timesteps={count})" for profile_name in EXPECTED_PROFILES
+    # ]
     # profiles_list = [str(profile) for profile in results.list_profiles()]
     # assert set(profiles_list) == set(expected_profiles_list)
 
-    # TODO: domain values are equal for all profiles
+    # TODO: check domain values are equal for all profiles (annulus can end before the end of the well domain...)
     # TODO: add the waterdepth + air gap
     measured_depths = results.get_profile_curve("pressure", element_name, -1).domain.GetValues("m")
 
-    # TODO: remember to get only active annulus
     # build annuli data
     annuli_output: Dict[str, Any] = {}
     annulus_index = 0
@@ -66,20 +59,49 @@ def generate_output_results(
     ):
         annuli_output[str(annulus_index)] = {}
         annuli_output[str(annulus_index)]["MD"] = measured_depths.tolist()
-        annuli_output[str(annulus_index)]["temperature"] = (
+        temperature = {}
+        temperature["start"] = (
+            results.get_profile_curve(temperature_profile_name, element_name, 0)
+            .image.GetValues(TEMPERATURE_UNIT)
+            .tolist()
+        )
+        temperature["final"] = (
             results.get_profile_curve(temperature_profile_name, element_name, -1)
             .image.GetValues(TEMPERATURE_UNIT)
             .tolist()
         )
-        annuli_output[str(annulus_index)]["pressure"] = (
+        pressure = {}
+        pressure["start"] = (
+            results.get_profile_curve(pressure_profile_name, element_name, 0)
+            .image.GetValues(PRESSURE_UNIT)
+            .tolist()
+        )
+        pressure["final"] = (
             results.get_profile_curve(pressure_profile_name, element_name, -1)
             .image.GetValues(PRESSURE_UNIT)
             .tolist()
         )
+        annuli_output[str(annulus_index)]["temperature"] = temperature
+        annuli_output[str(annulus_index)]["pressure"] = pressure
         annulus_index += 1
+    production_tubing = {
+        "temperature": {
+            "final": (
+                results.get_profile_curve("mixture temperature", element_name, -1)
+                .image.GetValues(TEMPERATURE_UNIT)
+                .tolist()
+            )
+        },
+        "pressure": {
+            "final": (
+                results.get_profile_curve("pressure", element_name, -1)
+                .image.GetValues(PRESSURE_UNIT)
+                .tolist()
+            )
+        },
+    }
     return {
         "annuli": annuli_output,
         "MD": measured_depths.tolist(),
-        # TODO: fill data for tubing here
-        "production_tubing": {},
+        "production_tubing": production_tubing,
     }
