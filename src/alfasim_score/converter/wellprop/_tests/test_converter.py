88b3c63e2c0bd0e7e2d7f59654f5e6243ad2b202
Gabriel Antão <gabrielfoa@esss.com>
2025-04-09 18:09:39 -0300

    Add the generation of the pvt table buffer
    PWPA-2506

--- src/alfasim_score/converter/wellprop/_tests/test_converter.py
+++ src/alfasim_score/converter/wellprop/_tests/test_converter.py
@@ -7,21 +7,16 @@ from alfasim_score.converter.wellprop.wellprop_pvt_table_converter import Wellpr
 
 
 @pytest.mark.parametrize("fluid_name", ("N2_LIFT", "DFLT_FCBA_9.90", "DFLT_FPBNA_OLEO_NACL_10.00"))
-def test_wellprop_to_pvt_converter(
+def test_generate_pvt_table_content(
     shared_datadir: Path,
     file_regression: FileRegressionFixture,
     fluid_name: str,
 ) -> None:
     converter = WellpropToPvtConverter(str(shared_datadir / fluid_name), fluid_name)
     converter.read_wellprop_files()
-    pvt_table = converter.convert_pvt_table_data()
-    # print(pvt_table)
-    # with open(datadir / f"{fluid_name}.tab", "w") as f:
-    #     f.write(pvt_table.getvalue())
-
-    # output_file_name = f"{fluid_name}.tab"
-
-    # file_regression.check(GetFileContents(datadir / output_file_name), extension=".tab")
+    pvt_data = converter.convert_pvt_table_data()
+    content = converter.generate_pvt_table_content(pvt_data)
+    file_regression.check(content.getvalue(), extension=".tab")
 
 
 @pytest.mark.parametrize("fluid_name", ("N2_LIFT", "DFLT_FCBA_9.90", "DFLT_FPBNA_OLEO_NACL_10.00"))
@@ -32,4 +27,4 @@ def test_convert_pvt_table_data(
 ) -> None:
     converter = WellpropToPvtConverter(str(shared_datadir / fluid_name), fluid_name)
     converter.read_wellprop_files()
-    dataframe_regression.check(converter.convert_pvt_table_data())
+    dataframe_regression.check(converter.convert_pvt_table_data().table)
